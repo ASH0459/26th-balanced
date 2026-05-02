caa88a3571563a86655affe830fb9e2b2805d3c6
ASH <2392569963@qq.com>
2026-05-02 22:24:25 +0800

    为颜色宏定义添加注释

--- Users/Application/App_UI/App_UI_Task.h
+++ Users/Application/App_UI/App_UI_Task.h
@@ -5,15 +5,15 @@
 extern "C" {
 #endif
 
-#define RED_BLUE_COLOR 0
-#define YELLOW_COLOR 1
-#define GREEN_COLOR 2
-#define ORANGE_COLOR 3
-#define PURPLE_COLOR 4
-#define PINK_COLOR 5
-#define CYAN_COLOR 6
-#define BLACK_COLOR 7
-#define WHITE_COLOR 8
+#define RED_BLUE_COLOR 0 /* 红蓝主色: 实际显示跟随己方阵营自动切换为红色或蓝色 */
+#define YELLOW_COLOR 1   /* 黄色 */
+#define GREEN_COLOR 2    /* 绿色 */
+#define ORANGE_COLOR 3   /* 橙色 */
+#define PURPLE_COLOR 4   /* 紫色 */
+#define PINK_COLOR 5     /* 粉色 */
+#define CYAN_COLOR 6     /* 青色 */
+#define BLACK_COLOR 7    /* 黑色 */
+#define WHITE_COLOR 8    /* 白色 */
 
 void UI_Task(void *argument);
 
